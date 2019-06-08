91c459400871b0d166b33df95d7288b49402f002
andy <andres.moreno@heig-vd.ch>
2019-06-08 10:33:51 +0200

    end stage 4, we print the name and the lvl of the adventurers

---
+++ docker-images/apache-php-image/content/js/adventurers.js
@@ -0,0 +1,19 @@
+$(function() {
+ 
+	console.log("Loading adventurers");
+
+	function loadAdventurers(){
+		$.getJSON("/api/adventurers/", function(adventurers){
+			console.log(adventurers);
+			var message = "Nobody is here";
+			if(adventurers.length > 0){
+				message = adventurers[0].name + " Lvl:" + adventurers[0].level;
+			}
+			$("#labo").text(message);
+		});
+	};
+
+
+	loadAdventurers();
+	setInterval(loadAdventurers, 2000);
+});
